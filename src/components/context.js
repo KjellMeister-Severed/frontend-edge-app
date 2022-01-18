824933c3791c023bc965cca74b15c789926de588
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2022-01-18 21:40:54 +0100

    work it

--- src/components/context.js
+++ src/components/context.js
@@ -1,5 +1,5 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-export const socket = socketio.connect("http://localhost:3000");
+export const socket = socketio.connect("https://edge-server-6.herokuapp.com/");
 export const SocketContext = React.createContext();
